1089d8f5ba666fd3a5fcdc3a3878066e898a52ce
zakumann <samuel742able@gmail.com>
2023-11-11 21:30:58 +0900

    commit next-reviews

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+export default function RootLayout({ children }){
+    return (
+        <html lang="en">
+            <body>
+                <header>
+                    [header]
+                </header>
+                <main>
+                    {children}
+                </main>
+                <footer>
+                    [footer]
+                </footer>
+            </body>
+        </html>
+    );
+}
