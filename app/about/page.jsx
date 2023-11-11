0ad8cc290c7120b0de1c0e140ebfa2c0c5f77259
zakumann <samuel742able@gmail.com>
2023-11-12 00:26:11 +0900

    section 2

---
+++ app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function aboutPage(){
+    return (
+        <>
+            <h1>About</h1>
+            <p>A website created to learn Next.js</p>
+        </>
+    )
+}
---
+++ app/reviews/hollow-knight/page.jsx
@@ -0,0 +1,8 @@
+export default function hollowKnight(){
+    return(
+        <>
+            <h1>HollowKnight</h1>
+            <p>This page is for HollowKnight.</p>
+        </>
+    );
+}
