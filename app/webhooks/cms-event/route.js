4440be78bc8720fa8ea7cd2b680c5277b8f96495
zakumann <samuel742able@gmail.com>
2023-11-25 22:36:05 +0900

    section 12

--- app/webhooks/cms-event/route.js
+++ app/webhooks/cms-event/route.js
@@ -1,7 +1,12 @@
+import { CACHE_TAG_REVIEWS } from "@/lib/reviews";
+import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function POST(request){
     const payload = await request.json();
-    console.log('payload:', payload);
+    if (payload.model === 'review'){
+        revalidateTag(CACHE_TAG_REVIEWS);
+    }
+    console.log('revalidated:', CACHE_TAG_REVIEWS);
     return new Response(null, { status: 204 });
 }
