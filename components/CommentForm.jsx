f8eb7fe369d83d5b167c16042daed196b3722244
zakumann <samuel742able@gmail.com>
2023-12-08 13:35:00 +0900

    12082023

--- components/CommentForm.jsx
+++ components/CommentForm.jsx
@@ -1,6 +1,23 @@
-export default function CommentForm({ title }) {
-  return (
-    <form className="border bg-white flex flex-col gap-2 mt-3 px-3 py-2 reounded">
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { createComment } from "@/lib/comments";
+
+export default function CommentForm({ slug, title }) {
+    async function action(formData){
+        'use server';
+        const message = await createComment({
+            slug,
+            user: formData.get('user'),
+            message: formData.get('message'),
+        });
+        console.log('created:', message);
+        revalidatePath(`/reviews/${slug}`);
+        redirect(`/reviews/${slug}`);
+    }
+
+    return (
+    <form action={action}
+        className="border bg-white flex flex-col gap-2 mt-3 px-3 py-2 reounded">
         <p className="pb-1">
             Already played <strong>{title}</strong>? Have your say!
         </p>
@@ -8,13 +25,17 @@ export default function CommentForm({ title }) {
             <label htmlFor="userField" className="shrink-0 w-32">
                 Your name
             </label>
-            <input id="userField" className="border px-2 py-1 rounded w-48"/>
+            <input id="userField" name="user"
+                className="border px-2 py-1 rounded w-48"
+            />
         </div>
         <div className="flex">
             <label htmlFor="messageField" className="shrink-0 w-32">
                 Your comment
             </label>
-            <textarea id="messageField" className="border px-2 py-1 rouned w-full"/>
+            <textarea id="messageField" name="message"
+                className="border px-2 py-1 rouned w-full"
+            />
         </div>
         <button type="submit"
             className="bg-orange-800 rouned px-2 py-1 self-center
--- scripts/test-db.mjs
+++ scripts/test-db.mjs
@@ -4,16 +4,16 @@ const db = new PrismaClient({
   log: [{ emit: 'stdout', level: 'query' }],
 });
 
-// const comment = await db.comment.create({
-//   data: {
-//     slug: 'stardew-valley',
-//     user: 'Bob',
-//     message: 'Test message 3',
-//   },
-// });
-// console.log('created:', comment);
-
-const comments = db.comment.findMany({
-  where: { slug: 'fall-guys' },
+const comment = await db.comment.create({
+  data: {
+    slug: 'fall-guys',
+    user: 'Dan',
+    message: 'A Test message',
+  },
 });
-console.log('comments:', comments);
+console.log('created:', comment);
+
+// const comments = db.comment.findMany({
+//   where: { slug: 'fall-guys' },
+// });
+// console.log('comments:', comments);
